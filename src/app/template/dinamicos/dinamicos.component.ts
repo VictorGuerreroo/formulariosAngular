56f11e849110852702287c51c645531a61089098
Victor <v.g.m_@hotmail.com>
2021-03-06 13:03:55 +0100

    Fin formularios por template

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styleUrls: ['./dinamicos.component.css']
+  styleUrls: ['./dinamicos.component.css'],
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Paco',
+    favoritos: [
+      { id: 4, nombre: 'juan' },
+      { id: 2, nombre: 'pepi' },
+    ],
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  eliminar( index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
+  guardar() {
+    console.log('posteado');
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( {...nuevoFavorito})
+    this.nuevoJuego="";
+  }
 }
