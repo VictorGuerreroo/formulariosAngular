25d6a2f3939b9aa998357697bb7d13b816a98d52
Victor <v.g.m_@hotmail.com>
2021-03-08 12:33:23 +0100

    Finalizado

---
+++ src/app/auth/pages/registro/registro.component.ts
@@ -0,0 +1,79 @@
+import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,} from '@angular/forms'
+
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css'],
+})
+export class RegistroComponent implements OnInit {
+  
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '',[Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)],],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
+    password: ['', [ Validators.required, Validators.minLength(6)]],
+    password2: ['', [ Validators.required]],
+  },{
+    validators: [ this.validatorService.camposIguales('password', 'password2')]
+  });
+
+  get emailErrorMsg(): string {
+
+    const errors = this.miFormulario.get('email')?.errors;
+    if( errors?.required) {
+    return 'Email es obligatorio';
+    } else if (errors?.pattern) {
+      return 'Introduzca un correo válido';
+    }else if (errors?.emailTomado) {
+      return 'Ya existe el email';
+    }
+    return'';
+  }
+
+  
+
+  constructor(private fb: FormBuilder, private validatorService: ValidatorService, private emailValidator: EmailValidatorService) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Tu prima la Paca',
+      email: 'laPaca@hotPaca.com',
+      username:'Paca la flaca',
+      password: '123456',
+      password2:'123456'
+    });
+  }
+
+  campoNoValido(campo: string) {
+    return (
+      this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+    );
+  }
+
+  emailRequired() {
+    return this.miFormulario.get('email')?.errors?.required &&
+           this.miFormulario.get('email')?.touched;
+  }
+
+  emailFormato() {
+    return this.miFormulario.get('email')?.errors?.pattern &&
+           this.miFormulario.get('email')?.touched;
+  }
+
+  emailTomado() {
+    return this.miFormulario.get('email')?.errors?.emailTomado &&
+           this.miFormulario.get('email')?.touched;
+  }
+
+  submitFormulario() {
+    this.miFormulario.markAllAsTouched();
+  }
+
+  
+}
